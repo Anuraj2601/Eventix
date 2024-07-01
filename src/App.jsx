156f87b496370db76d57174e37371cb07228af1d
Uththara Jayawardana <gaveuththara1999@gmail.com>
2024-07-01 22:40:24 +0530

    Done some changes in the ClubMemberNav.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Exploreclub from './pages/president/Exploreclub';
 import StudentAllClubs from './pages/student/StudentAllClubs';
-
+import ClubMemberAllClubs from './pages/member/ClubMemberAllClubs';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Exploreclub />} />
+        <Route path='/club/:name' element={<ClubMemberAllClubs />} ></Route>
         <Route path="/student" element={<StudentAllClubs />} />       
       </Routes>
     </Router>
