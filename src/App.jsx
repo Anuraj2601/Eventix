38d01528ab98b9fc74deea9a6e7fc3ef851fa11c
anuraj <anurajselvasothy@gmail.com>
2024-07-03 20:37:36 +0530

    Meeting,Announcement commits

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Exploreclub from './pages/president/Exploreclub';
 import StudentAllClubs from './pages/student/StudentAllClubs';
 import ClubDetails from './pages/president/ClubDetails';
+import  Carousel1  from './components/Carousel1';
 // Import other pages if necessary
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Exploreclub />} />
-        <Route path='/club/:name' element={<ClubDetails />} ></Route>
+        <Route exact path="/club" element={<Exploreclub />} />
+        <Route path='/club/:name' element={<ClubDetails /* sname = 'sname' cname ='cname' image = 'image' */ />} ></Route>
         <Route path='/student' element={<StudentAllClubs/>}></Route>
+        <Route path='/Carousel' element={<Carousel1 />}></Route>
       </Routes>
     </Router>
   );
