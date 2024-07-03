1e49445e0caf83ade7cdce891c6cc57d06c2dba0
Uththara Jayawardana <gaveuththara1999@gmail.com>
2024-07-03 23:59:44 +0530

    Created UI for the Announcemnets tab

--- src/components/tabs/Announcements.jsx
+++ src/components/tabs/Announcements.jsx
@@ -23,7 +23,7 @@ import {
 
 import { IconButton } from "@material-tailwind/react";
 
-export function Announcements() {
+/*export function Announcements() {
   return (
     
     <div>
@@ -32,96 +32,56 @@ export function Announcements() {
       <h2>Announcements</h2>
       <p>Announcements of Club Member...</p>
      </div>
-     /* <Carousel className="rounded-xl">
-      <Card className="mt-6 w-96 bg-stone-950">
-      <CardHeader color="blue-gray" className="relative h-48">
-        <img
-          src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-          alt="card-image"
-        />
-      </CardHeader>
-      <CardBody>
-        <Typography variant="h5" color="blue-gray" className="mb-2">
-          UI/UX Review Check
-        </Typography>
-        <Typography>
-          The place is close to Barceloneta Beach and bus stop just 2 min by
-          walk and near to &quot;Naviglio&quot; where you can enjoy the main
-          night life in Barcelona.
-        </Typography>
-      </CardBody>
-      <CardFooter className="pt-0">
-        <Button className="bg-white text-stone-950">Read More</Button>
-      </CardFooter>
-    </Card>
-
-    <Card className="mt-6 w-96 bg-stone-950">
-      <CardHeader color="blue-gray" className="relative h-48">
-        <img
-          src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-          alt="card-image"
-        />
-      </CardHeader>
-      <CardBody>
-        <Typography variant="h5" color="blue-gray" className="mb-2">
-          UI/UX Review Check
-        </Typography>
-        <Typography>
-          The place is close to Barceloneta Beach and bus stop just 2 min by
-          walk and near to &quot;Naviglio&quot; where you can enjoy the main
-          night life in Barcelona.
-        </Typography>
-      </CardBody>
-      <CardFooter className="pt-0">
-        <Button className="bg-white text-stone-950">Read More</Button>
-      </CardFooter>
-    </Card>
-
-    <Card className="mt-6 w-96 bg-stone-950">
-      <CardHeader color="blue-gray" className="relative h-48">
-        <img
-          src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-          alt="card-image"
-        />
-      </CardHeader>
-      <CardBody>
-        <Typography variant="h5" color="blue-gray" className="mb-2">
-          UI/UX Review Check
-        </Typography>
-        <Typography>
-          The place is close to Barceloneta Beach and bus stop just 2 min by
-          walk and near to &quot;Naviglio&quot; where you can enjoy the main
-          night life in Barcelona.
-        </Typography>
-      </CardBody>
-      <CardFooter className="pt-0">
-        <Button className="bg-white text-stone-950">Read More</Button>
-      </CardFooter>
-    </Card>
-
-    <Card className="mt-6 w-96 bg-stone-950">
-      <CardHeader color="blue-gray" className="relative h-48">
-        <img
-          src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-          alt="card-image"
-        />
-      </CardHeader>
-      <CardBody>
-        <Typography variant="h5" color="blue-gray" className="mb-2">
-          UI/UX Review Check
-        </Typography>
-        <Typography>
-          The place is close to Barceloneta Beach and bus stop just 2 min by
-          walk and near to &quot;Naviglio&quot; where you can enjoy the main
-          night life in Barcelona.
-        </Typography>
-      </CardBody>
-      <CardFooter className="pt-0">
-        <Button className="bg-white text-stone-950">Read More</Button>
-      </CardFooter>
-    </Card>
-    </Carousel>  */}
+    }
 </div>
 
   );
-};
+};*/
+
+import React from 'react';
+
+export function Announcements() {
+  const events = [
+    {
+      name: "The Board elections of term 24/25 will be commenced from period 05.06.2024 - 09.06.2024, all the club members are invited to participate in the voting.",
+      
+    },
+    {
+      name: "The Board elections of term 24/25 will be commenced from period 05.06.2024 - 09.06.2024, all the club members are invited to apply as a candidate.",
+      joinLink2: "https://example.com/join-event2",
+    },
+  ];
+
+  return (
+    <div className="flex justify-center items-center flex-col p-4 rounded-lg" style={{ backgroundColor: '#1E1E1E' }}>
+      {events.map((event, index) => (
+        <div key={index} className="w-full rounded-full p-2 flex flex-col mb-4" style={{ backgroundColor: '#171717' }}>
+          <div className="ml-4">
+            <h3 className="text-md text-white mb-2 ">{event.name}</h3>
+            <div className="flex justify-between items-center mt-2">
+            
+              <div className="flex items-center space-x-4">
+                             
+               {event.joinLink2 && (
+                  <a 
+                    href={event.joinLink2} 
+                    className="text-white text-sm border px-4 py-1 rounded-full" 
+                   
+                    style={{ backgroundColor: '#5C690A', borderColor: '#5C690A', marginLeft: '850px'}}
+
+                  >
+                    Apply
+                  </a>
+                  
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+
+
