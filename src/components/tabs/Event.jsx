af8de9dc84401f899ba922ee2656ea0d9e42385c
Uththara Jayawardana <gaveuththara1999@gmail.com>
2024-07-03 15:46:18 +0530

    Implement the UI for the Events tab

--- src/components/tabs/Event.jsx
+++ src/components/tabs/Event.jsx
@@ -11,31 +11,64 @@ const CurrentBoard = () => {
 
 export default CurrentBoard*/
 
-import { Carousel } from "@material-tailwind/react";
-import {
-    Card,
-    CardHeader,
-    CardBody,
-    CardFooter,
-    Typography,
-    Button,
-  } from "@material-tailwind/react";
-
-import { IconButton } from "@material-tailwind/react";
+import React from 'react';
+import MadhackImg from '../../assets/events/madhack.png';
 
 export function Event() {
-  return (
-    
-    <div>
-     {
-     <div>
-      <div className="flex justify-center items-center flex-col p-4 rounded-lg" style={{ backgroundColor: '#1E1E1E' }}>
-      <h2>Events</h2>
-      <p>Events of Club Member...</p>
-      </div>
-     </div>
-     }
-</div>
+  const events = [
+    {
+      name: "MadHack 3.0",
+      image: MadhackImg,
+      date: "05.06.2024",
+      link: "https://example.com/join-oc",
+      linkText: "Join OC",
+    },
+    {
+      name: "MadHack 3.0",
+      image: MadhackImg,
+      date: "05.06.2024",
+      link: "https://example.com/join-oc",
+      linkText: "Join OC",
+    },
+    {
+      name: "MadHack 3.0",
+      image: MadhackImg,
+      date: "05.06.2024",
+      link: "https://example.com/join-oc",
+      linkText: "Join OC",
+    },
+    {
+      name: "MadHack 3.0",
+      image: MadhackImg,
+      date: "05.06.2024",
+      link: "https://example.com/join-oc",
+      linkText: "Join OC",
+    },
+  ];
 
+  return (
+    <div className="flex justify-center items-center flex-col p-4 rounded-lg" style={{ backgroundColor: '#1E1E1E' }}>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full max-w-screen-lg">
+        {events.map((event, index) => (
+          <div key={index} className="bg-gray-800 rounded-lg p-4">
+            <div className="relative">
+            <img src={event.image} alt={event.name} className="w-full h-56 object-cover rounded-lg" />
+              <div className="w-10 h-10 absolute bottom-0 right-0 m-2 p-1 rounded-full flex justify-center items-center" style={{ backgroundColor: '#AEC90A', color: '#000' }}>
+              <span className="font-extrabold text-lg">➔</span>
+              </div>
+            </div>
+            <div className="mt-4">
+              <h3 className="text-xl font-semibold text-white">{event.name}</h3>
+              <div className="flex justify-between items-center mt-2">
+                <a href={event.link} className="text-md " style={{ color: '#AEC90A' }}>
+                  {event.linkText}
+                </a>
+                <span className="text-gray-400">{event.date}</span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
-};
+}
