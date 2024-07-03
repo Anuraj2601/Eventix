07f8ac899a72f0392cde70e0050497db3a2da200
DinithiDinushika <dinithidinu689@gmail.com>
2024-07-03 18:55:20 +0530

    student all clubs

--- src/components/StudentClubCard.jsx
+++ src/components/StudentClubCard.jsx
@@ -3,17 +3,32 @@ import { Button } from "@material-tailwind/react";
 import { RiOpenArmLine } from "react-icons/ri";
 import { IoMdBookmark } from "react-icons/io";
 
-const StudentClubCard = () => {
+const StudentClubCard = ({club}) => {
   return (
     <div className='bg-[#0B0B0B] p-6 w-2/5 rounded-2xl'>
         <div className="flex items-center justify-between mb-4">
             <div className='flex gap-4'>
-                <img src="src/assets/clubs/csa.jpeg" alt="" className='w-16 h-16 rounded-md'/>
+                <img src={club.image} alt="" className='w-16 h-16 rounded-md'/>
                 <div className="reg">
-                    <p className='mb-2 tracking-wide'>(CSA) Student Chapter</p>
+                    <p className='mb-2 tracking-wide'>{club.club_name}</p>
                     <div className='flex gap-3'>
-                        <RiOpenArmLine className='text-[#AEC90A]' size={20}/>
-                        <span className='text-[#AEC90A]'>Registrations are Open</span>
+                        {
+                            club.reg_status === 'yes' && (
+                            <>
+                                <RiOpenArmLine className='text-[#AEC90A]' size={20} />
+                                <span className='text-[#AEC90A]'>Registrations are Open</span>
+                            </>
+                            )
+                        }
+                        {
+                            club.reg_status === 'no' && (
+                            <>
+                                <RiOpenArmLine className='text-[#5C690A]' size={20} />
+                                <span className='text-[#5C690A]'>Registrations are Closed</span>
+                            </>
+                            )
+                        }
+                       
                     </div>  
                 </div>
             </div>
@@ -22,13 +37,11 @@ const StudentClubCard = () => {
 
         </div>
         <div className="mb-4">
-            <p className='text-[#F5F5F5] '>Get the opportunity to learn from industry professionals, prepare
-            for certifications like CISA and CRISC and and network with 
-            professionals in the field.</p>
+            <p className='text-[#F5F5F5] '>{club.description}</p>
         </div>
         <div className="flex items-center justify-end gap-4">
             <Button className="bg-white text-[#0B0B0B] px-4 py-2 rounded-3xl font-medium">Ignore</Button>
-            <Button className="bg-[#AEC90A] text-[#0B0B0B] px-4 py-2 rounded-3xl font-medium">Register</Button>
+            <Button className= {`text-[#0B0B0B] px-4 py-2 rounded-3xl font-medium ${club.reg_status === "yes" ? 'bg-[#AEC90A]' : 'bg-[#AEC90A80]'}`} >Register</Button>
         </div>
         
     </div>
