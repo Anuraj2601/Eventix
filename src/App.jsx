63b6d6be531981b11c50414fb8308f027da88161
Dhanushika Dharmasena <dhanushikadharmasena456@gmail.com>
2024-07-05 07:45:19 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Exploreclub from './pages/president/Exploreclub';
 import StudentAllClubs from './pages/student/StudentAllClubs';
 import ClubDetails from './pages/president/ClubDetails';
+import EventRequests from './pages/admin/EventRequests';
 // Import other pages if necessary
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/" element={<Exploreclub />} />
         <Route path='/club/:name' element={<ClubDetails />} ></Route>
         <Route path='/student' element={<StudentAllClubs/>}></Route>
+        <Route path='/admin' element={<EventRequests/>}></Route>
       </Routes>
     </Router>
   );
