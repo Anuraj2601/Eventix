2e02becd10b79e331d7b1e89ce34a372ab2007e9
cusherahkugan <111173285+cusherahkugan@users.noreply.github.com>
2024-07-24 19:30:07 +0530

    image import error solved

--- src/components/StudentClubCard.jsx
+++ src/components/StudentClubCard.jsx
@@ -4,6 +4,17 @@ import { RiOpenArmLine } from "react-icons/ri";
 import { IoMdBookmark } from "react-icons/io";
 import { useNavigate } from 'react-router-dom';
 
+// Import images
+import ieeeImg from '../assets/clubs/ieee.png';
+import rotaractImg from '../assets/clubs/rotaract.png';
+import acmImg from '../assets/clubs/acm.png';
+import isacaImg from '../assets/clubs/isaca1.png';
+import wieImg from '../assets/clubs/wie.png';
+import msImg from '../assets/clubs/ms.png';
+import wicysImg from '../assets/clubs/wicys.png';
+import rekhaImg from '../assets/clubs/rekha.png';
+import pahasaraImg from '../assets/clubs/pahasara1.png';
+
 const StudentClubCard = () => {
     const navigate = useNavigate();
 
@@ -13,81 +24,72 @@ const StudentClubCard = () => {
             name: "IEEE Student Chapter",
             reg_status: "yes",
             description: "The IEEE Student Chapter promotes the advancement of technology. Members can participate in technical seminars, project exhibitions, and networking events.",
-            image: "../src/assets/clubs/ieee.png",
+            image: ieeeImg,
             sname: "ieee",
-
         },
         {
             id: "1",
             name: "Rotaract Club of UCSC",
             reg_status: "yes",
             description: "The Rotaract Club of UCSC, part of Rotary International District 3220, empowers youth to enact positive change locally and globally.",
-            image: "../src/assets/clubs/rotaract.png",
-            sname: "rotract",
+            image: rotaractImg,
+            sname: "rotaract",
         },
         {
             id: "2",
             name: "ACM Student Chapter",
             reg_status: "no",
             description: "The ACM Student Chapter aims to advance computing as a science and profession. Activities include coding competitions, guest lectures, and career development workshops.",
-            image: "../src/assets/clubs/acm.png",
+            image: acmImg,
             sname: "acm",
         },
-       
         {
             id: "4",
             name: "ISACA Student Group",
             reg_status: "no",
             description: "The Debate Society aims to improve public speaking and critical thinking skills through regular debates, public speaking workshops, and competitions.",
-            image: "../src/assets/clubs/isaca1.png",
+            image: isacaImg,
             sname: "isaca",
-
         },
         {
             id: "5",
             name: "(IEEE WIE) IEEE Women in Engineering",
             reg_status: "yes",
             description: "The IEEE Women in Engineering (WIE) Student Branch at the University of Colombo School of Computing strives to enhance women’s participation and empowerment in electrical and electronic engineering.",
-            image: "../src/assets/clubs/wie.png",
+            image: wieImg,
             sname: "wie",
-
         },
-       
         {
             id: "7",
             name: "Mechatronic Society Of UCSC",
             reg_status: "no",
             description: "The Mechatronic Society Of UCSC focuses on sustainability and environmental awareness. Activities include clean-up drives, tree planting, and educational workshops.",
-            image: "../src/assets/clubs/ms.png",
+            image: msImg,
             sname: "mech",
-
         },
         {
             id: "8",
             name: "Women in Cybersecurity",
             reg_status: "no",
             description: "This club is part of the Institute of Electrical and Electronics Engineers (IEEE) and focuses on all aspects of computer science and engineering.",
-            image: "../src/assets/clubs/wicys.png",
+            image: wicysImg,
             sname: "cyb",
-
         },
         {
             id: "9",
             name: "Rekha",
             reg_status: "yes",
             description: "Get the opportunity to learn from industry professionals, prepare for certifications like CISA and CRISC and and network with professionals in the field.",
-            image: "../src/assets/clubs/rekha.png",
+            image: rekhaImg,
             sname: "rekha",
-
         },
         {
             id: "3",
             name: "Pahasara Club (Innovation and Creativity)",
             reg_status: "yes",
             description: "The Pahasara Club offers a platform for photography enthusiasts to enhance their skills through workshops, photo walks, and exhibitions.",
-            image: "../src/assets/clubs/pahasara1.png",
+            image: pahasaraImg,
             sname: "pahasara",
-
         },
     ];
 
