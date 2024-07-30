94e90d38373230c6d25854c74adadb01607d0b71
Sivarasa Kokularajh <138663878+sivarasa-kokularajh@users.noreply.github.com>
2024-07-30 13:29:35 +0530

    scrollbar

--- src/components/MainCarousel.jsx
+++ src/components/MainCarousel.jsx
@@ -1,11 +1,36 @@
+import { useEffect, useRef } from 'react';
 import madhack from '../assets/madhack.jpg';
 
 const images = [madhack, madhack, madhack, madhack, madhack, madhack, madhack, madhack];
 
 const MainCarousel = () => {
+    const carouselRef = useRef(null);
+
+    useEffect(() => {
+        const scrollInterval = setInterval(() => {
+            if (carouselRef.current) {
+                const { scrollLeft, scrollWidth, clientWidth } = carouselRef.current;
+
+                if (scrollLeft + clientWidth >= scrollWidth - 3) {
+                    carouselRef.current.scrollTo({ left: 0, behavior: 'smooth' });
+                } else {
+                    carouselRef.current.scrollBy({
+                        left: 300,
+                        behavior: 'smooth',
+                    });
+                }
+            }
+        }, 100);
+
+        return () => clearInterval(scrollInterval);
+    }, []);
+
     return (
         <div className="bg-none mb-20 w-[82vw] ml-32 h-[62vh]">
-            <div className="flex space-x-10 overflow-x-scroll scrollbar-hide">
+            <div 
+                ref={carouselRef} 
+                className="flex space-x-10 overflow-x-scroll scrollbar-hide"
+            >
                 {images.map((image, index) => (
                     <img
                         key={index}
