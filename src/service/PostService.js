38ae7fc9d52d43837b2b25881609d7f752325dfe
Dinithi Dinushika <dinithidinu689@gmail.com>
2024-09-01 02:29:25 +0530

    post service

---
+++ src/service/PostService.js
@@ -0,0 +1,142 @@
+import axios from "axios";
+
+class PostService{
+
+    static BASE_URL = "http://localhost:8080";
+
+    static async savePost (name, position, description, post_image, post_image, post_status, club_id, published_user_id, token){
+
+        try{
+            const formData = new FormData();
+            formData.append('data', new Blob([JSON.stringify({
+                name,
+                position,
+                description,
+                post_status,
+                club_id,
+                published_user_id
+
+            })], {type: 'application/json'}));
+            formData.append('file', post_image);
+
+            const headers = {
+                'Content-Type': 'multipart/form-data',
+                'Authorization': `Bearer ${token}`
+            };
+
+            const response = await axios.post(`${PostService.BASE_URL}/president/savePost`, formData, { headers });
+
+            return response.data;
+
+        }catch(err){
+            throw err;
+        }
+
+
+    }
+
+    static async getAllPosts(token) {
+        try {
+
+          const response = await axios.get(`${PostService.BASE_URL}/president/getAllPosts`,
+            {
+
+                headers: { Authorization: `Bearer ${token}` }
+
+            }
+          );
+
+          return response.data;
+
+        } catch (err) {
+          throw err;
+        }
+    }
+
+    static async getPostById(post_id, token) {
+        try{
+    
+            const response = await axios.get(`${PostService.BASE_URL}/president/getPost/${post_id}`,
+                {
+                    headers: {Authorization: `Bearer ${token}`}
+                }
+            )
+
+            return response.data;
+            
+        }catch(err){
+            throw err;
+        }
+    }
+
+
+
+    static async updatePost (post_id, name, position, description, post_image, post_image, post_status, club_id, published_user_id, token){
+
+        try{
+            const formData = new FormData();
+            formData.append('data', new Blob([JSON.stringify({
+                post_id,
+                name,
+                position,
+                description,
+                post_status,
+                club_id,
+                published_user_id
+
+            })], {type: 'application/json'}));
+            formData.append('file', post_image);
+
+            const headers = {
+                'Content-Type': 'multipart/form-data',
+                'Authorization': `Bearer ${token}`
+            };
+
+            const response = await axios.put(`${PostService.BASE_URL}/president/updatePost`, formData, { headers });
+
+            return response.data;
+
+        }catch(err){
+            throw err;
+        }
+
+
+    }
+
+
+    static async deleteSponsor(post_id, token) {
+        try {
+          const response = await axios.delete(`${PostService.BASE_URL}/president/deletePost/${post_id}`,
+
+            {
+              headers: { Authorization: `Bearer ${token}` },
+            }
+            
+          );
+
+          return response.data;
+
+        } catch (err) {
+          throw err;
+        }
+    }
+    
+
+
+
+
+    static logout() {
+        localStorage.removeItem("token");
+        localStorage.removeItem("role");
+    }
+    
+    static isAuthenticated() {
+        const token = localStorage.getItem("token");
+        return !!token;
+    }
+
+
+
+}
+
+export default PostService;
