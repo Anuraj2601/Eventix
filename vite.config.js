028d6b63c41375c7d9c23abd70d9276c13e5b7e6
anuraj <anurajselvasothy@gmail.com>
2024-11-30 18:39:14 +0530

    changes for deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
+  build: {
+    outDir: 'dist', // Ensure it's set to 'dist'
+  },
+  base: '/',
   /* build: {
   rollupOptions: {
     output: {
