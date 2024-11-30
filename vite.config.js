16daaf9f08cf3fbee9f27634e1d4928a58060cc8
anuraj <anurajselvasothy@gmail.com>
2024-11-30 17:41:18 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
+  build: {
   rollupOptions: {
     output: {
       // Ensure proper handling of routes
       entryFileNames: 'index.html'
     }
   }
+}
   /* build: {
     sourcemap: false, // Disable source maps
   }, */
